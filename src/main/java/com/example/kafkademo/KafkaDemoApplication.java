5c6168e042acf014e6b012d97aefa38fefefe861
Marcos Vinicius <marcosab.vinicius@gmail.com>
2023-01-14 22:15:31 -0300

    add consumer code

---
+++ src/main/java/com/example/kafkademo/KafkaDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.kafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@SpringBootApplication
+public class KafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemoApplication.class, args);
+	}
+
+}
